96fd1e60432200d3ab31940732c239947dcf8a55
Nadishi <nadavdishi@gmail.com>
2024-03-02 19:57:27 +0200

    initial arrangements

--- AdverseAgent.py
+++ AdverseAgent.py
@@ -1,9 +1,12 @@
 from agent import agent
+from alpha_beta import alphabeta_max_h
 
 
 class AdverseAgent(agent):
-    def __init__(self, x, y):
+    def __init__(self, x, y, node=None):
         super().__init__(x, y)
+        self.node = node
 
     def act(self, init_graph):
+        alphabeta_max_h()
         self.move_request = (self.X, self.Y - 1)
