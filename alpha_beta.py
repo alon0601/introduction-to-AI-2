91799b4e8acbff7cde3f2629845f5c63c6cc5ecd
Nadishi <nadavdishi@gmail.com>
2024-03-02 18:16:34 +0200

    initial arrangements

--- alpha_beta.py
+++ alpha_beta.py
@@ -9,7 +9,7 @@ def alphabeta_max_h(current_game, _heuristic, depth=5):
     # add code here
     alpha = -math.inf
     beta = math.inf
-    return maximin(current_game, alpha, beta, depth)
+    return minimax(current_game, alpha, beta, depth, True)
 
 
 def alphabeta_min_h(current_game, _heuristic, depth=5):
@@ -17,38 +17,36 @@ def alphabeta_min_h(current_game, _heuristic, depth=5):
     h = _heuristic
     alpha = -math.inf
     beta = math.inf
-    return minimax(current_game, alpha, beta, depth)
+    return minimax(current_game, alpha, beta, depth, False)
 
 
-def maximin(current_game, alpha, beta, depth):
+def minimax(current_game, alpha, beta, depth, maximize):
     global h
     if current_game.is_terminal() or current_game.is_game_finished() or depth == 0:
         return h(current_game, current_game.get_first_player()) - h(current_game, current_game.get_second_player()), current_game
-    v = -math.inf
-    moves = current_game.get_moves(current_game)
-    for move in moves:
-        mx, next_move = minimax(move, alpha, beta, depth - 1)
-        if v < mx:
-            v = mx
-            best_move = move
-        alpha = max(alpha, v)
-        if alpha >= beta:
-            return v, move
-    return v, best_move
 
+    if maximize:
+        v = -math.inf
+        moves = current_game.get_moves(current_game)
+        for move in moves:
+            mx, next_move = minimax(move, alpha, beta, depth - 1, False)
+            if v < mx:
+                v = mx
+                best_move = move
+            alpha = max(alpha, v)
+            if alpha >= beta:
+                break
+        return v, best_move
 
-def minimax(current_game, alpha, beta, depth):
-    global h
-    if current_game.is_terminal() or current_game.is_game_finished() or depth == 0:
-        return h(current_game, current_game.get_first_player()) - h(current_game, current_game.get_second_player()), current_game
-    v = math.inf
-    moves = current_game.get_moves(current_game)
-    for move in moves:
-        mx, next_move = maximin(move, alpha, beta, depth - 1)
-        if v > mx:
-            v = mx
-            best_move = move
-        beta = min(beta, v)
-        if alpha >= beta:
-            return v, move
-    return v, best_move
+    else:
+        v = math.inf
+        moves = current_game.get_moves(current_game)
+        for move in moves:
+            mx, next_move = minimax(move, alpha, beta, depth - 1, True)
+            if v > mx:
+                v = mx
+                best_move = move
+            beta = min(beta, v)
+            if alpha >= beta:
+                break
+        return v, best_move
